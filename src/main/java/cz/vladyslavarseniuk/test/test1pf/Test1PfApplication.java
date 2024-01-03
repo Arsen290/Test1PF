5e5d0d1e43efb29966057ced519a89ed77397fc3
Arsen290 <arsen290@ukr.net>
2024-01-03 16:54:02 +0100

    initialize the project

---
+++ src/main/java/cz/vladyslavarseniuk/test/test1pf/Test1PfApplication.java
@@ -0,0 +1,13 @@
+package cz.vladyslavarseniuk.test.test1pf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test1PfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test1PfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/vladyslavarseniuk/test/test1pf/Test1PfApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.vladyslavarseniuk.test.test1pf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test1PfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
