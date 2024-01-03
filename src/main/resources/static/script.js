7fea04a221b068b07b83cfa8b68cb208aa5d57b2
Arsen290 <arsen290@ukr.net>
2024-01-03 19:08:58 +0100

    change view format in .js and add comments

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,21 +1,26 @@
 async function getResponse(){
+    // Get the element from DIV
     const exchengeRatesElement = document.getElementById('exchangeRates');
     const apiUrl = 'https://webapi.developers.erstegroup.com/api/csas/public/sandbox/v2/rates/exchangerates?web-api-key=c52a0682-4806-4903-828f-6cc66508329e';
 
     try{
-        const response = await fetch(apiUrl); // Get the response from the API endpoint.
-        const data = await response.json(); // Get the data from the response as JSON
-        console.log(data); // Log the data to the console
+        // Get the response from the API endpoint.
+        const response = await fetch(apiUrl);
+        // Get the data from the response as JSON
+        const data = await response.json();
+        // Log the data to the console to control
+        console.log(data);
 
         // Create a list of the currencies
         const ul = document.createElement('ul');
+        // Iterate the data and create a list item for each currency
         data.forEach(rate => {
             const li = document.createElement('li');
-            li.textContent = `${rate.shortName} - ${rate.valMid}`;
+            li.textContent = `${rate.shortName} - ${rate.currMid}`;
             ul.appendChild(li);
         });
 
-        // Очистить контейнер и добавить новый список
+        // Append the currencies to the DOM
         exchengeRatesElement.innerHTML = '';
         exchengeRatesElement.appendChild(ul);
     }
