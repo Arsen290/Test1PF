417e4d5d2851fff34aed74717dcc0a37fd68bb6b
Arsen290 <arsen290@ukr.net>
2024-01-03 23:18:39 +0100

    change method getRateCards in service , add stream to get object from body of request in JSON format and by java stream API make list of entity RateCard from RateCardDTO to save it in db

--- src/main/java/cz/vladyslavarseniuk/test/test1pf/service/RateCardService.java
+++ src/main/java/cz/vladyslavarseniuk/test/test1pf/service/RateCardService.java
@@ -21,9 +21,9 @@ public class RateCardService {
         if (useDb) {
             return rateCardRepository.findAll();
         } else {
-            // GET HTTP request from system České spořitelny
+
             String apiUrl = "https://webapi.developers.erstegroup.com/api/csas/public/sandbox/v2/rates/exchangerates?web-api-key=c52a0682-4806-4903-828f-6cc66508329e";
-            // Take response from CS API
+            // GET HTTP request from system České spořitelny  and get response in JSON format of massive of objects
             ResponseEntity<RateCardDTO[]> responseEntity = new RestTemplate().getForEntity(apiUrl, RateCardDTO[].class);
 
             // DTO to Entity conversion
