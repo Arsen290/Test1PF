ab2081be133e296f9bdc33b4d8fb186b415d1b98
Arsen290 <arsen290@ukr.net>
2024-01-03 21:25:22 +0100

    View details of specific rate, change detail-script and correct details.html

--- src/main/resources/static/detail-script.js
+++ src/main/resources/static/detail-script.js
@@ -1,11 +1,51 @@
 // Find the element by its ID
 var myElement = document.getElementById('detailsRate');
+// Get the value of the attribute
+var shortnameValue = myElement.getAttribute('value');
+//Control value
+console.log("Value attr th:value:",shortnameValue);
 
-// Check if the element is found
-if (myElement) {
-    // Get the value of the 'th:value' attribute
-    var shortnameValue = myElement.getAttribute('value');
-    console.log('Value attr th:value:', shortnameValue);
-} else {
-    console.log('Element not found.');
+async function getDetailRate(shortName){
+    // Get the element from DIV
+    const exchengeRatesElement = document.getElementById('detailsRate');
+    const apiUrl = 'https://webapi.developers.erstegroup.com/api/csas/public/sandbox/v2/rates/exchangerates?web-api-key=c52a0682-4806-4903-828f-6cc66508329e';
+
+    try{
+        // Get the response from the API endpoint.
+        const response = await fetch(apiUrl);
+        // Get the data from the response as JSON
+        const data = await response.json();
+        // Log the data to the console to control
+        console.log(data);
+
+        // Find the currency by its shortName
+        const currency = data.find(rate => rate.shortName === shortName);
+
+        //Create a list of the currencies
+        if (currency) {
+                    exchengeRatesElement.innerHTML = `
+                        <h2>${currency.name} (${currency.shortName})</h2>
+                        <p>Country: ${currency.country}</p>
+                        <p>Move: ${currency.move}</p>
+                        <p>Amount: ${currency.amount}</p>
+                        <p>Value buy: ${currency.valBuy}</p>
+                        <p>Value sell: ${currency.valSell}</p>
+                        <p>Middle value: ${currency.valMid}</p>
+                        <p>Currency buy: ${currency.currBuy}</p>
+                        <p>Currency sell: ${currency.currSell}</p>
+                        <p>Currency mid: ${currency.currMid}</p>
+                        <p>Version: ${currency.version}</p>
+                        <p>CNB Mid: ${currency.cnbMid}</p>
+                        <p>ECB Mid: ${currency.ecbMid}</p>
+                        <p>Date: ${currency.validFrom}</p>
+                    `;
+                } else {
+                    exchengeRatesElement.innerHTML = 'Currency not found.';
+                }
+    }
+    catch(error){
+        console.error(error);
+    }
 }
+
+getDetailRate(shortnameValue);
