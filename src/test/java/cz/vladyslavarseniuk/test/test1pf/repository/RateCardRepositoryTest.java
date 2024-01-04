400c20b37023d6f399e59fba0fd1e881ea3a12ad
Arsen290 <arsen290@ukr.net>
2024-01-04 22:41:12 +0100

    change test to repository

--- src/test/java/cz/vladyslavarseniuk/test/test1pf/repository/RateCardRepositoryTest.java
+++ src/test/java/cz/vladyslavarseniuk/test/test1pf/repository/RateCardRepositoryTest.java
@@ -18,22 +18,18 @@ class RateCardRepositoryTest {
     @Autowired
     private RateCardRepository rateCardRepositoryUnderTest;
 
-    @MockBean
-    private RateCardRepository rateCardRepositoryMock;
-
     @Test
     void ItShouldCheckIfCardExistsByShortName() {
-        // Given
+        //give
         RateCard rateCard = new RateCard(1L,"USD", LocalDateTime.of(2022, 4, 26, 0, 0, 0),
                 "Dollar", "USA", 1.07, 1.0, 22.0, 23.6, 22.798, 22.228, 23.368, 22.798, 1, 22.725, 1.075
         );
-        when(rateCardRepositoryMock.findByShortName("USD")).thenReturn(rateCard);
-
-        // When
+        rateCardRepositoryUnderTest.save(rateCard);
+        //when
         RateCard rateCardUnderTest = rateCardRepositoryUnderTest.findByShortName("USD");
-
-        // Then
+        //then
         assertNotNull(rateCardUnderTest, "RateCard with shortName 'USD' should exist");
         assertEquals(rateCard, rateCardUnderTest, "Saved RateCard should be equal to retrieved RateCard");
+        rateCardRepositoryUnderTest.delete(rateCard);
     }
 }
