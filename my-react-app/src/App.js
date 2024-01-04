c4b23819364b87a3fd025f4a719091753135a45d
Arsen290 <arsen290@ukr.net>
2024-01-04 13:15:38 +0100

    write routes to change url in React app

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,16 +1,21 @@
-// src/App.js
 import React from 'react';
 import Details from './components/DetailsRate.js';
 import ExchangeRates from './components/ExchangeRates';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-      <div>
-        <ExchangeRates />
-        <Details />
-      </div>
-  );
+    return (
+        // Router component to define routes
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/details" element={<Details />} />
+                    <Route path="/" element={<ExchangeRates />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
